44f1abb565733e1432ccbfdddd99f64eacb1a28a
Mark Frymire <mark.frymire@icloud.com>
2024-05-13 11:20:41 -0400

    Make the cpp code standalone

--- hello/src/hello_ext.cpp
+++ hello/src/hello_ext.cpp
@@ -1,6 +1,14 @@
 #include <nanobind/nanobind.h>
 
+char* say_hi_cpp() {
+    return "Whassup, sucka?";
+}
+
+char* say_bye_cpp() {
+    return "See ya. Wouldn't wanna be ya.";
+}
+
 NB_MODULE(hello_impl, m) {
-    m.def("say_hi_cpp", []() { return "Whassup, sucka?"; });
-    m.def("say_bye_cpp", []() { return "So nice to see you (go)."; });
+    m.def("say_hi_cpp", &say_hi_cpp);
+    m.def("say_bye_cpp", &say_bye_cpp);
 }
--- nanobind_poetry_demo/say_hi.py
+++ nanobind_poetry_demo/say_hi.py
@@ -1,4 +1,4 @@
-from hello_ext.hello_impl import say_hi_cpp
+from hello_ext.hello_impl import say_hi_cpp, say_bye_cpp
 from numpy import pi
 
 
@@ -7,8 +7,13 @@ def say_hi() -> None:
     print("hello_ext (c++ via nanobind) says hi...", say_hi_cpp())
 
 
+def say_bye() -> None:
+    print("hello_ext (c++ via nanobind) says hi...", say_bye_cpp())
+
+
 def main():
     say_hi()
+    say_bye()
 
 
 if __name__ == '__main__':
